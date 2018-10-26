b80375b234a9d0b129c59630657da59aa5266c42
Jonathan Skopp <jonathan.skopp@gmail.com>
2018-10-26 23:33:45 +0200

    Nummer 1
    update

---
+++ 1.FS/AUP/blatt3/Aufgabe2/src/anagrammtest.java
@@ -0,0 +1,18 @@
+
+public class anagrammtest {
+/**
+ * @author Awurin
+ */
+	
+	
+	// Eingaben werden über die Komandozeile getätigt
+	
+	public static void main(String[] args) {
+	
+		int ascii = (int) 'a';
+		System.out.println(ascii);
+	
+
+	}
+
+}
